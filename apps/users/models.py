811d216eccd22288e3935e54d9571e3f3672389e
demon <demonovdemonic@gmail.com>
2026-03-26 22:31:58 +0500

    пользователь + jwt

--- apps/users/models.py
+++ apps/users/models.py
@@ -3,15 +3,18 @@ from django.core.exceptions import ValidationError
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
+    ROLE_CHOICES = [('student', 'Студент'), ('teacher', 'Учитель'), ('admin', 'Админ'), ('parent', 'Родитель')]
     role = models.CharField(
         max_length=40,
-        choices=[('student', 'Студент'), ('teacher', 'Учитель'), ('admin', 'Админ')],
+        choices=ROLE_CHOICES,
         default= 'student'
     )
     level = models.PositiveIntegerField(default=1)
     experience = models.PositiveIntegerField(default=0)
     avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
 
+    email = models.CharField(blank=True, null=True)
+    
     def add_experience(self, points):
         self.experience += points
         self.save()
---
+++ apps/users/serializers.py
@@ -0,0 +1,45 @@
+from rest_framework import serializers
+from django.contrib.auth.password_validation import validate_password
+from .models import User
+
+
+class RegisterSerializers(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    password_confirm = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'password', 'password_confirm', 'role'] 
+    
+    def create(self, validated_data):
+       return User.objects.create_user(**validated_data)
+    
+    def validate_role(self, value):
+        allowed_roles = ['student', 'parent']
+        if value not in allowed_roles:
+            raise serializers.ValidationError("Только для студентов/родителей")
+        return value
+    
+    def validate(self, data):
+        if data['password'] != data['password_confirm']:
+            raise serializers.ValidationError("Пароли не совпадают")
+        return data
+    
+    def create(self, validated_data):
+        validated_data.pop('password_confirm')
+        user = User.objects.create_user(**validated_data)
+        return user
+    
+class ProfileSerializer(serializers.ModelSerializer):
+    role_display = serializers.CharField(source='get_role_display', read_only=True)
+
+    class Meta:
+        model = User
+        fields = [
+            'id', 'username', 'first_name', 'last_name', 'role', 'role_display', 'level', 'experience', 'date_joined'
+        ]
+        read_only_fields = ['id', 'date_joined']
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField() 
+    password = serializers.CharField(write_only=True)
