99163ee589c8a6c8b9932a0cf993c712bb25dbb2
PavelitelDemonov <demonovdemonic@gmail.com>
2026-03-27 19:21:14 +0500

    Рабочий докер+ прикрутил документацию (#7)

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,11 @@
 FROM python:3.12-slim
-COPY . /app
-WORKDIR /app
-RUN pip install -r requirements.txt
-CMD python manage.py migrate && python manage.py runserver 0.0.0.0:8000
+
+WORKDIR /code
+
+COPY requirements.txt .
+RUN pip install --no-cache-dir -r requirements.txt
+
+COPY . .
+
+EXPOSE 8000
+CMD ["python", "manage.py", "runserver", "0.0.0.0:8000"]
--- config/urls.py
+++ config/urls.py
@@ -18,12 +18,23 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
+from drf_spectacular.views import (
+    SpectacularAPIView, 
+    SpectacularSwaggerView, 
+    SpectacularRedocView
+)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('apps.users.urls')),
     path('auth/courses/', include('apps.courses.urls')),
     path('', TemplateView.as_view(template_name='index.html'), name='home'),
+
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema')),
+    path('api/redoc/', SpectacularRedocView.as_view(url_name='schema')),
+
    #path('api/', include("")),
 ]
 
